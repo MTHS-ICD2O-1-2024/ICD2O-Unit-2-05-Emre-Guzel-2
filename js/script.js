f4d8eded61767344c584004cc927653a3067fe3a
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2025-03-25 09:58:37 -0400

    fixed the error

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-// Created by: Emre Guzel
-// Created on: March 20  2025
-// This file contains the JS functions for index.html
-
 "use strict"
 
 function CalucalteTaxIncome() {
-  // Setting the varibles
-  const TaxAmount = 0.18
-  let HourlyWorked = parseInt(document.getElementById("hourly-worked").value)
-  let HourlyWage = parseInt(document.getElementById("hourly-wage").value)
-  let grossPay = HourlyWorked * HourlyWage
-  let TakeHomePay = grossPay - TaxAmount
+  // Setting the variables
+  const TaxAmount = 0.18;
+  let HourlyWorked = parseFloat(document.getElementById("hourly-worked").value);
+  let HourlyWage = parseFloat(document.getElementById("hourly-wage").value);
+  let TakeHomePay = (HourlyWorked * HourlyWage)
+  let Taxes = (HourlyWorked * HourlyWage) * TaxAmount;
 
-  // clacualtoins 
+  // Correct currency formatting
+  let formattedPrice = new Intl.NumberFormat('en-CA', {
+    style: 'currency',
+    currency: 'CAD',
+  });
+
+  // Display calculations correctly with formatted currency
   document.getElementById("result").innerHTML =
-    `Take home Pay: $${TakeHomePay.toFixed(2)} <br> Taxes: $${(grossPay * TaxAmount).toFixed(2)}`;
+    `Take-home Pay: ${formattedPrice.format(TakeHomePay)} <br> 
+    Taxes: ${formattedPrice.format(Taxes)}`;
 }
