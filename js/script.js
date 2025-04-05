08e03a40e8f053abfd4342ec8e5eacc4d30f07ed
default <yunus.emre.guzel@mths.ca>
2025-04-05 19:28:06 +0000

    fix the style

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 "use strict"
 
-function CalucalteTaxIncome() {
+function calucalteTaxIncome() {
   // Setting the variables
-  const TaxAmount = 0.18;
-  let HourlyWorked = parseFloat(document.getElementById("hourly-worked").value);
-  let HourlyWage = parseFloat(document.getElementById("hourly-wage").value);
-  let TakeHomePay = (HourlyWorked * HourlyWage)
-  let Taxes = (HourlyWorked * HourlyWage) * TaxAmount;
+  const taxAmount = 0.18;
+  let hourlyWorked = parseFloat(document.getElementById("hourly-worked").value);
+  let hourlyWage = parseFloat(document.getElementById("hourly-wage").value);
+  let takeHomePay = (hourlyWorked * hourlyWage) *(1.00 - taxAmount)
+  let taxes = (hourlyWorked * hourlyWage) * taxAmount;
 
   // Correct currency formatting
   let formattedPrice = new Intl.NumberFormat('en-CA', {
@@ -16,6 +16,6 @@ function CalucalteTaxIncome() {
 
   // Display calculations correctly with formatted currency
   document.getElementById("result").innerHTML =
-    `Take-home Pay: ${formattedPrice.format(TakeHomePay)} <br> 
-    Taxes: ${formattedPrice.format(Taxes)}`;
+    `Take-home Pay: ${formattedPrice.format(takeHomePay)} <br> 
+    Taxes: ${formattedPrice.format(taxes)}`;
 }
